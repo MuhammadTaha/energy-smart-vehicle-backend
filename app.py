accb652a52c6bb1109ebd2a593457e93757c3601
MuhammadTaha <m.taha@ventzke-media.de>
2019-09-22 19:31:18 +0200

    minor cleaning

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
 @app.route('/route-based', methods=['GET', 'POST'])
 def route_based():
     return request.data
